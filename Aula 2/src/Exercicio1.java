5b6e37b150b638a00d8d8691828b1b17518b1411
Luiz Madaloz <8luiz1@gmail.com>
2026-09-15 18:37:37 -0300

    um commit

---
+++ Aula 2/src/Exercicio1.java
@@ -0,0 +1,13 @@
+public class Exercicio1 {
+    static void main(String[] args) {
+        Avaliacao Joao = new  Avaliacao(9,8,10);
+
+        System.out.println("A média ficou: " + Joao.mediaNotas());
+        System.out.println("A média ponderada ficou: " + Joao.mediaPonderada());
+
+
+
+    }
+
+
+}
